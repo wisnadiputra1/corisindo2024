8304aec92e60859c6621b3fadce1e78d5161b502
krisnasdna <krisnasedana2402@gmail.com>
2023-07-22 16:52:50 +0800

    instalasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'bg-100': '#1E1E1E',
+      'bg-200': '#2D2D2D',
+      'bg-300': '#454545',
+      'text-primary': '#ffffff',
+      'text-secondary': '#9E9E9E',
+      'accent-100': '#006FFF',
+      'accent-200': '#E1FFFF',
+      'primary-100': '#0085FF',
+      'primary-200': '#69B4FF',
+      'primary-300': '#E0FFFF',
+    },
+    fontFamily: {
+      jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      monument: ['Monument', 'serif'],
+    },
   },
   plugins: [],
 }
